40e68689214a9de5f316ba2a0ff2eae48e2b2ad1
SerhioGacura <sgatfor@gmail.com>
2020-02-09 19:55:22 +0300

    1

--- less_14/less_14/MyExtensions.cs
+++ less_14/less_14/MyExtensions.cs
@@ -5,11 +5,13 @@ using System.Text;
 namespace less_14
 {
     public static class MyExtensions
-    {
+    {   
+    /// <summary>
+    /// Cut any string for a 5 simbols lenght
+    /// </summary>
         public static string StringCutter(this string str)
         {
             string retStr=null;
-            char[] arr = str.ToCharArray();
             for (int i = 0; i < 5; i++)
             {
                 retStr += str[i]; 
